4a8ae2e858dfef2f1943afe9e39cc6f35880c2d4
Atar Mani Pandey <atar.pandey>
2022-07-14 14:29:41 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,6 @@
+import {sum,sub,mul,div} from './index.js' //.js need to use else error
+
+console.log('2 + 2 ',sum(1,2))
+console.log('2 - 2 ',sub(1,2))
+console.log('2 * 2 ',mul(1,2))
+console.log('2 / 2 ',div(1,2))
